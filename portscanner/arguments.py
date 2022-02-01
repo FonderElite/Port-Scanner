1dd18307237c273c183abc9113ca6e705d0d106f
Gian Paris Agsam <s1170044@usls.edu.ph>
2022-02-01 02:10:04 +0000

    Update arguments.py

--- portscanner/arguments.py
+++ portscanner/arguments.py
@@ -2,3 +2,4 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-t','--target',metavar='',help='IP of the target or domain. (Ex. google.com)')
 parser.add_argument('-pl','--portlimit',metavar='',help='Number limit of the port scanning,(Ex. 1000)')
+args = parser.parse_args()
